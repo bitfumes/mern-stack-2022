f52e8936918fc288934052d50d66516c4d73de35
sarthaksavvy <sarthak@bitfumes.com>
2022-09-17 18:31:35 +0530

    12 MERN stack - design form

--- client/src/components/TransactionForm.js
+++ client/src/components/TransactionForm.js
@@ -1,17 +1,50 @@
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+
 import * as React from "react";
 
 export default function TransactionForm() {
+  function handleChange() {}
+
   return (
     <Card sx={{ minWidth: 275, marginTop: 10 }}>
       <CardContent>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Typography variant="h6">Add New Transaction</Typography>
+        <form>
+          <TextField
+            sx={{ marginRight: 5 }}
+            id="outlined-basic"
+            label="Amount"
+            size="small"
+            variant="outlined"
+          />
+          <TextField
+            sx={{ marginRight: 5 }}
+            id="outlined-basic"
+            label="Description"
+            size="small"
+            variant="outlined"
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Transaction Date"
+              inputFormat="MM/DD/YYYY"
+              onChange={handleChange}
+              renderInput={(params) => (
+                <TextField sx={{ marginRight: 5 }} size="small" {...params} />
+              )}
+            />
+          </LocalizationProvider>
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </form>
       </CardContent>
     </Card>
   );
