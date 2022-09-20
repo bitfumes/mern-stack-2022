3533eadb3e7ce9d78d62dd67f8ede98c13468fa0
sarthaksavvy <sarthak@bitfumes.com>
2022-09-20 20:12:43 +0530

    add _id to transaction list when we do monthy group

--- server/controller/TransactionController.js
+++ server/controller/TransactionController.js
@@ -13,6 +13,7 @@ export const index = async (req, res) => {
             amount: "$amount",
             description: "$description",
             date: "$date",
+            _id: "$_id",
           },
         },
         totalExpenses: { $sum: "$amount" },
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import routes from "./routes/index.js";
 
 dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
